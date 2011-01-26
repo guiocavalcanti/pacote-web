98b931095132e75233b7127efc0ad121758df1c7
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2011-01-26 11:23:54 -0300

    Exercício e conteúdo da aula

---
+++ js/aula/eventos/js/common.js
@@ -0,0 +1,38 @@
+jQuery(function(){
+    /*
+    $("#acao").click(function(){
+        $("p").slideUp("slow");
+        // $(this) elemento que foi clickado
+        $(this).css("backgroundColor", "red");
+    });
+    */
+
+    /*
+    $("#acao").toggle(
+      function(){
+        $("p").slideUp("slow");
+      },
+      function(){
+        $("p").slideDown("slow");
+      },
+      function(){
+        $(this).css("backgroundColor", "red");
+      }
+    );
+    */
+
+    /*
+    $("#acao").click(function(){
+        $("p").hide(2000,
+          function(){
+          $(this).after("<span>Escondido</span>");
+        });
+    });
+    */
+
+    $("#acao").click(function(){
+      var clone = $("p").clone();
+      $(this).after(clone);
+      clone.css("backgroundColor", "blue");
+    });
+});
---
+++ js/aula/intro/common.js
@@ -0,0 +1,25 @@
+jQuery(function(){
+    // $("p:even").css("backgroundColor", "red")
+    // $("p:odd").css("backgroundColor", "red")
+
+    // Pegando Valor do campo
+    $("input[type=text]").val();
+
+    // Mudando valor do campo
+    $("input[type=text]").val("Novo");
+
+    // O mesmo vale p/ a func. html
+    // $("input").html("<p>Novo</p>")
+    // $("input").html()
+    
+});
+
+/*
+* $(document).ready(function(){
+
+// Código executado qdo a pag. é carregada
+// ou seja, qdo o DOM está pronto
+$(".ingredientes").slideUp();
+
+});
+*/
